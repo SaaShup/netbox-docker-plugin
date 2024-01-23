4990fd561aa39223a8a4d4d3b57780d5d602026a
Vincent Simonin <vincent.simonin@ext.ec.europa.eu>
2024-01-23 17:55:54 +0100

    🚀 Add deployment process

--- netbox_docker_plugin/__init__.py
+++ netbox_docker_plugin/__init__.py
@@ -9,7 +9,7 @@ class NetBoxDockerConfig(PluginConfig):
     name = "netbox_docker_plugin"
     verbose_name = " NetBox Docker Plugin"
     description = "Manage Docker"
-    version = "0.0.2"
+    version = "1.0.0-rc1"
     base_url = "docker"
 
 
