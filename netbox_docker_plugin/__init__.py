8ede9812fad75f9b281fdcd5275f8c8a86d5c248
Vincent Simonin <vincent.simonin@ext.ec.europa.eu>
2024-01-31 09:52:58 +0100

    🔖 Upgrade version

--- netbox_docker_plugin/__init__.py
+++ netbox_docker_plugin/__init__.py
@@ -9,7 +9,7 @@ class NetBoxDockerConfig(PluginConfig):
     name = "netbox_docker_plugin"
     verbose_name = " NetBox Docker Plugin"
     description = "Manage Docker"
-    version = "1.0.0-rc6"
+    version = "1.0.0-rc7"
     base_url = "docker"
 
 
