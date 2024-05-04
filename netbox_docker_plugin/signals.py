c4ece6b65a3c0316c27f34a9cb72df5720bfc857
David Delassus <david.jose.delassus@gmail.com>
2024-05-04 14:44:43 +0200

    :sparkles: restrict container actions at model level

--- netbox_docker_plugin/signals.py
+++ netbox_docker_plugin/signals.py
@@ -1,6 +1,6 @@
 """ Hooks on Django model signals. """
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from utilities.exceptions import AbortRequest
 
@@ -14,4 +14,19 @@ def prevent_deleting_running_containers(instance, **_kwargs):
     if not instance.can_delete:
         raise AbortRequest(
             f"The container {instance.name} is running, please stop it before deletion"
+        )
+
+
+@receiver(pre_save, sender=Container)
+def restrict_container_actions(instance, **_kwargs):
+    """ Check if the container's state is correct for the given action """
+
+    if any([
+        instance.operation == "start" and not instance.can_start,
+        instance.operation == "restart" and not instance.can_restart,
+        instance.operation == "stop" and not instance.can_stop,
+        instance.operation == "recreate" and not instance.can_recreate,
+    ]):
+        raise AbortRequest(
+            f"Cannot {instance.operation} container {instance.name} in state {instance.state}"
         )
