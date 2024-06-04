85ff8f44090fb782444ff1e4cf6e81b924ed8a0d
David Delassus <david.jose.delassus@gmail.com>
2024-06-04 17:42:47 +0200

    :lock: escape logs/exec api erations outputs

--- netbox_docker_plugin/api/renderers.py
+++ netbox_docker_plugin/api/renderers.py
@@ -4,11 +4,14 @@ from django.utils.encoding import smart_str
 from rest_framework import renderers
 
 
-class PlainTextRenderer(renderers.BaseRenderer):
-    """ text/plain renderer """
+def make_content_type_renderer(content_type: str, api_format: str):
+    class ContentTypeRenderer(renderers.BaseRenderer):
+        """ content type renderer """
 
-    media_type = "text/plain"
-    format = "txt"
+        media_type = content_type
+        format = api_format
 
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        return smart_str(data, encoding=self.charset)
+        def render(self, data, accepted_media_type=None, renderer_context=None):
+            return smart_str(data, encoding=self.charset)
+
+    return ContentTypeRenderer
