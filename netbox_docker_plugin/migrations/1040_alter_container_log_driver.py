eb499e277745a9a41691c53731ea9734c31bf489
natmaitre <natanvenierfr@gmail.com>
2025-07-03 15:18:39 +0200

    fix file

--- netbox_docker_plugin/migrations/1040_alter_container_log_driver.py
+++ netbox_docker_plugin/migrations/1040_alter_container_log_driver.py
@@ -9,13 +9,13 @@ class Migration(migrations.Migration):
     """Migration file"""
 
     dependencies = [
-        ("netbox_docker_plugin", "1039_container_cmd"),
+        ("netbox_docker_plugin", "1039_alter_port_public_port"),
     ]
 
     operations = [
         migrations.AlterField(
             model_name="container",
             name="log_driver",
-            field=models.CharField( max_length=32, null=True, blank=True),
+            field=models.CharField(max_length=32, null=True, blank=True),
         ),
     ]
